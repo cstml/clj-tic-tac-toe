ba243d011406738c2fd993af45125e38790a31da
cstml <29694576+cstml@users.noreply.github.com>
2021-01-19 17:10:07 +0000

    update: row calculation works

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -4,27 +4,76 @@
 (defn print-board
   "Prints the board and returns the state"
   [state]
-  (map #(do (println %) %) state))
+  (println "Entered the board")
+  (doall (map #(do (println %) %) state)))
 
-(defn -main
-  "Classic game of Tic Tac Toe"
-  [& args]
-  (println "Hello, World!")
-  (let [state [[nil nil nil] [nil nil nil] [nil nil nil]]]
-    (loop [state state
-           player :x]
-      (cond
-        (final-state state) (player-win player)
-        true (do
-               (read)
-               (print-board state)
-               (recur (make-a-move state (next-player player))
-                        (next-player player)))))))
+(defn change-state
+  "Returns a new state vector with the swapped state"
+  [state player row col]
+  (do(println col "-" row )
+     (assoc state row (assoc (nth state row) col player))))
+
+(defn swapper
+  "A swapper that helps calculate if there are 3 of a kind on any given row"
+  [x]
+  (case x
+    :o 1
+    :x -1
+    0))
+
+(defn swapper-winner
+  "A swapper that only swaps the winning sums of 3 or -3
+  Takes in"
+  [x]
+  (case x
+    3 true
+    -3 true
+    false))
+
+(defn row-colapser
+  "Calculates the total points per row received"
+  [state]
+  (map (fn [x] (reduce + x)) state)) 
+
+(defn state-converter
+  "converts a received vetor to numerical values"
+  [x]
+  (do (map (fn [x] (map (fn [x] (swapper x)) x)) x)))
+
+(defn row-calculate
+  "Calculates if there are 3 in a row"
+  [state]
+  (reduce (fn [x y] (or x y))(map swapper-winner (row-colapser (state-converter state)))))
+
+(defn final-state [state]
+  (cond
+    (row-calculate state) true
+    true false))
+
+(defn player-win [player]
+  (println "Player " player " Wins!!!"))
+
+(defn next-player [player]
+  (if (= player :x)
+    :o
+    :x))
+
+(defn initial-state []
+  [[nil nil nil] [nil nil nil] [nil nil nil]])
+
+
+(defn test-state []
+  [[:x nil nil] [:x nil nil] [:x nil nil]])
+
+
+(defn test-state-2 []
+  [[:x :x :x] [:x nil nil] [:x nil nil]])
+
 
 (defn make-a-move
   "Makes a move on the board
   :returns a board state
-
+  
   For testing purposes it has two arrities one with a move already inserted
   and another one where the move is read from the stdin
   
@@ -32,29 +81,30 @@
   ([state player] ;; the normal input one
    (loop [state state
           player player]
-     (let [result (make-a-move state player (read))]
+     (println "Hello")
+     (let [result (make-a-move state player (dec(int(read))))]
        (cond
          (= result :error) (recur state player) 
          true result ))))
   ([state player move] ;; the testing purposes one
    (let [row (int(/ move 3))
          col (int(mod move 3))]
+     (do (println row "-" col))
      (case (nth (nth state col) row)
        nil (change-state state player row col)
        :error))))
 
-(defn change-state
-  "Returns a new state vector with the swapped state"
-  [state player row col]
-  (assoc state row (assoc (nth state row) col player)))
 
-(defn final-state [state]
-  false)
-
-(defn player-win [player]
-  (println "Player " player " Wins!!!"))
-
-(defn next-player [player]
-  (if (= player :x)
-    :y
-    :x))
+(defn -main
+  "Classic game of Tic Tac Toe"
+  [& args]
+  (println "Hello, World!")
+  (let [state (initial-state)]
+    (loop [state state
+           player :x]
+      (print-board state)
+      (cond
+        (final-state state) (player-win player)
+        true (do
+               (recur (make-a-move state (next-player player))
+                      (next-player player)))))))
