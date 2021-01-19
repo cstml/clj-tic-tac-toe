d4430fb2f688c03b84335ba2dc6c8cc28e724942
cstml <cstmlcodes@gmail.com>
2021-01-19 19:08:00 +0000

    update: new description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tic-tac-toe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Good ol' Tic-Tac-Toe, a favourite of mine that I try and implement all the time to test my understanding of new concepts"
+  ;:url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
